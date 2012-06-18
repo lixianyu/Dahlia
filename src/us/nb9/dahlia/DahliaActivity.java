7fe359428ab8b2fdb249d8aebfa465a258f4a927
Li Xianyu <lbonus@163.com>
2012-06-18 23:27:36 +0800

    base it.

---
+++ src/us/nb9/dahlia/DahliaActivity.java
@@ -0,0 +1,13 @@
+package us.nb9.dahlia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DahliaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
